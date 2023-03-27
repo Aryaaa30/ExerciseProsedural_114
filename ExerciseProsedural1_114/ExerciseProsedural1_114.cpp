bf7906dca4beb4d91df9a0de2659459e6f2a20b8
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-27 10:37:55 +0700

    Masukkan Variabel

--- ExerciseProsedural1_114/ExerciseProsedural1_114.cpp
+++ ExerciseProsedural1_114/ExerciseProsedural1_114.cpp
@@ -1,29 +1,66 @@
 #include <iostream>
 using namespace std;
 
+int a[20];
+int n;
+
 double rerata(double a, double b) {
     return (a + b) / 2;
 }
 
-string status(double rata) {
-    if (rata >= 70)
-        return "Lulus";
+string status(double rata, double nil) {
+    if (rata >= 70 || nil >= 80)
+        return "Asroni Lulus";
     else
-        return "Gagal";
+        return "Asroni Gagal";
 }
 
 string status2(double rata, double nil) {
     if (rata >= 70 || nil >= 80)
-        return "Lulus";
+        return "Lia Lulus";
     else
-        return "Gagal";
+        return "Lia Gagal";
 }
 
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
+string status3(double rata, double nil) {
+    if (rata >= 70 || nil >= 80)
+        return "Joko Lulus";
+    else
+        return "Joko Gagal";
 }
+
+void display() {
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Kandidat yang diterima" << endl;
+    cout << "======================" << endl;
+    cout << "2";
+   
+}
+
+void unsorted() {
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "Kandidat yang tidak diterima" << endl;
+    cout << "============================" << endl;
+    cout << "1";
+}
+
+int main() {
+    double nilM, nilBING;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBING;
+    cout << "status kelulusan = " << status(rerata(nilM, nilBING), nilM);
+    cout << "\nstatus kelulusan ke 2 = " << status2(rerata(nilM, nilBING), nilM);
+    cout << "\nstatus kelulusan ke 3 = " << status3(rerata(nilM, nilBING), nilM);
+    display();
+    unsorted();
+    return 0;
+}
+
+   
+
 
 
